6358c05fc410ceb45800a8b2122bdc619d4d8d87
Saepul Bahri <bahreesaepul1@gmail.com>
2024-05-02 06:58:20 +0700

    removed uuid and added temp id for each new task

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import {type ClassValue, clsx} from 'clsx';
+import {twMerge} from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function generateUniqueId() {
+  const timestamp = Date.now().toString(36); // Convert timestamp to base 36 string
+  const randomStr = Math.random().toString(36).substr(2, 5); // Generate random string
+  return `${timestamp}${randomStr}`; // Concatenate timestamp and random string
 }
