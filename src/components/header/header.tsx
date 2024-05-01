4d05c6e329d873616c64dedfc9290212d0fdfcc8
Saepul Bahri <bahreesaepul1@gmail.com>
2024-05-01 08:53:48 +0700

    added navigation tab

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,13 @@
+import Navigation from '../navigation/navigation';
 import LogoAndGreetings from './logo-and-greetings';
 import UserAvatarMenuDropdown from './user-avatar-menu-dropdown';
 
 export default function Header() {
   return (
-    <header className='flex justify-between items-center p-4 max-w-screen-xl mx-auto'>
+    <header className='flex justify-between relative items-center p-4 max-w-screen-xl mx-auto'>
       <LogoAndGreetings />
+
+      <Navigation />
 
       <UserAvatarMenuDropdown />
     </header>
---
+++ src/components/navigation/navigation.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import {BarChartIcon, ListBulletIcon} from '@radix-ui/react-icons';
+import Link from 'next/link';
+import {usePathname} from 'next/navigation';
+import {twMerge} from 'tailwind-merge';
+
+type LinkItem = {pathname: string; label: string; icons: React.ReactNode};
+
+const pagesLink: LinkItem[] = [
+  {pathname: '/app', label: 'Todo', icons: <ListBulletIcon />},
+  {pathname: '/app/statistics', label: 'Statistics', icons: <BarChartIcon />},
+];
+
+export default function Navigation() {
+  const pathname = usePathname();
+
+  return (
+    <nav className='absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2'>
+      <ul className='flex outline outline-1 outline-border rounded-full p-1 px-[2px]'>
+        {pagesLink.map((link) => (
+          <NavLink
+            key={link.label}
+            link={link}
+            isActive={pathname === link.pathname}
+            icons={link.icons}
+          />
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+type NavLinkProps = {
+  link: LinkItem;
+  isActive: boolean;
+  icons: React.ReactNode;
+};
+
+function NavLink(props: NavLinkProps) {
+  return (
+    <Link href={props.link.pathname}>
+      <li
+        className={twMerge(
+          'p-1 px-4 rounded-full text-sm flex gap-2 items-center',
+          props.isActive
+            ? 'bg-secondary/70 hover:bg-secondary/90'
+            : 'bg-background hover:bg-secondary/40'
+        )}
+      >
+        {props.icons}
+        {props.link.label}
+      </li>
+    </Link>
+  );
+}
